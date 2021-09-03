06f7b80c4ac70691384884bd5fadf5636e421d3f
sadrulj <jamil.sadrul@gmail.com>
2021-09-03 17:50:26 +0600

    final Assignment

--- js/app.js
+++ js/app.js
@@ -1,51 +1,40 @@
-const errorDiv = document.getElementById("error");
+const searchInputText = document.getElementById("error");
+const searchResult = document.getElementById("search-result");
+const searchTotal = document.getElementById("total-count");
+const spinner = document.getElementById("spinner");
+
 const searchField = () => {
   const searchInput = document.getElementById("search-input");
   searchText = searchInput.value;
   const url = `http://openlibrary.org/search.json?q=${searchText}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayResult(data.docs))
-    .finally(() => {
-      searchInput.value === "";
-    });
+    .then((data) => displayResult(data.docs, data));
+  searchInput.value = "";
   if (searchText === "") {
-    errorDiv.innerText = "Search Field cannot be Empty";
+    searchInputText.innerText = "Search Field cannot be Empty";
     return;
-  } else if (searchText.innerText === "") {
-    errorDiv.innerHTML = "NO Result Found";
-  } else if ((errorDiv.innerText = "")) {
-    return;
-  } else {
-    searchText.innerText === "";
-    errorDiv.style.display = "none";
   }
+  spinner.style.display = "block";
 };
-// searchField();
-const displayResult = (results) => {
-  const searchTotal = document.getElementById("total-count");
-  searchTotal.innerText = results.length;
+
+const displayResult = (results, resultData) => {
+  searchTotal.innerText = `Search Result: ${results.length} out of ${resultData.num_found}`;
   searchTotal.style.display = "block";
-  //   results.forEach((number) => {
-  //     console.log(number);
-  //   });
-  const searchResult = document.getElementById("search-result");
-  searchResult.innerHTML = "";
-  //   const resultCount = data.results;
-  //   const resultCountDiv = document.getElementById("total-count");
-  //   resultCount.forEach((resultValue) => {
-  //     const p = document.createElement("p");
-  //     p.innerText = `Search result: ${resultValue.text.lenght}`;
-  //     resultCountDiv.appendChild(p);
-  //   });
+  searchResult.textContent = "";
+  results?.forEach((result) => {
+    // Error Handing
+    if (results.message === "Not Found") {
+      searchInputText.innerText = "NO Result Found";
+    } else {
+      searchInputText.innerText = "";
+    }
 
-  results.forEach((result) => {
-    console.log(result);
-
+    // console.log(result);
     const div = document.createElement("div");
     div.classList.add("card");
     div.innerHTML = `
-   
+
     <div class="row my-3 rounded">
         <div class="col-md-4">
             <img src="https://covers.openlibrary.org/b/id/${result.cover_i}-M.jpg" class="img-fluid rounded-start my-3">
@@ -54,6 +43,7 @@ const displayResult = (results) => {
             <div class="card-body">
                 <h5 class="card-title">Title: ${result.title}</h5>
                 <p class="card-text">Author Name:${result.author_name}</p>
+                <p class="card-text">Publisher:${result.publisher}</p>
                 <p class="card-text"><small class="text-muted">First Publish Year: ${result.first_publish_year}</small></p>
             </div>
         </div>
@@ -63,4 +53,5 @@ const displayResult = (results) => {
 
     searchResult.appendChild(div);
   });
+  spinner.style.display = "none";
 };
